6d15be1679f963600fb7eafd3144e11c8b782b4a
Dell <Dell@DESKTOP-66GJR5M>
2022-05-30 17:32:12 +0530

    second commit

---
+++ Day3.1ClassPerTable/src/com/tns/Inheritance2/Employee.java
@@ -0,0 +1,54 @@
+package com.tns.Inheritance2;
+
+
+
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+@Entity
+@Table(name="EMPLOYEE2")
+@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)
+
+			public class Employee implements Serializable{
+	private static final long serialVersionUID=1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int ID;
+	private String name;
+	private double salary;
+	public int getID() {
+		return ID;
+	}
+	public void setID(int ID) {
+		this.ID =ID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+
+}
+
+
+
+
+
+
+
